d306162e1c757f04807e44e4833f725283f38fe0
elmarek <mareks@gmx.com>
2020-06-08 11:09:06 -0700

    HTML table and some JS

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,15 @@
+console.log("SRC worked!")
+
+const tic = document.getElementsByClassName('btn');
+
+let player = 0
+// tic.addEventListener('click', function(e) {
+//   if (player % 2 === 0){
+//     document.getElementById("btn").innerHTML = "X";
+//   } else {
+//     document.getElementById("btn").innerHTML = "O";
+//   }
+//   player++
+// })
 
 
