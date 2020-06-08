f30020818ebb119a775ab22b06ce635fff4c8f32
elmarek <mareks@gmx.com>
2020-06-08 15:10:05 -0700

    Complete Tic-Tac-Toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -12,6 +12,7 @@ let gameCheck = (() => {
   [board[2], board[5], board[8]],
   [board[0], board[1], board[2]],
   [board[3], board[4], board[5]],
+  [board[6], board[7], board[8]],
   [board[0], board[3], board[6]],
   [board[0], board[4], board[8]],
   [board[2], board[4], board[6]]];
